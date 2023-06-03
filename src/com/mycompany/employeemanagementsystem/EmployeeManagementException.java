95f019d0c0d445a735ce349e9abc0540cb495259
Janette Campbell <campbjp@hotmail.com>
2023-06-03 16:09:42 -0400

    Added EmployeeManagementException class to handle exceptions

---
+++ src/com/mycompany/employeemanagementsystem/EmployeeManagementException.java
@@ -0,0 +1,7 @@
+package com.mycompany.employeemanagementsystem;
+
+public class EmployeeManagementException extends Exception {
+    public EmployeeManagementException(String message) {
+        super(message);
+    }
+}
